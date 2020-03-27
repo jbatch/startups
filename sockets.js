fee12d43c681d0fb1e22542fff941c5d48b6e638
Josh Batchelor <josh_batch@live.com.au>
2020-03-28 00:01:16 +1100

    Supports clients reconnecting

--- sockets.js
+++ sockets.js
@@ -1,14 +1,38 @@
 const socketIo = require('socket.io');
+const { v4: uuid } = require('uuid');
 
 function configureSockets(appServer) {
   const server = socketIo(appServer);
 
+const clientData = {};
+const clients = [];
+
   server.on('connection', client => {
-    console.log('Client connect');
-    client.emit('news', { hello: 'world' });
-    client.on('my other event', function(data) {
-      console.log(data);
-    });
+    client.data = {};
+    client.on('handshake', handshake);
+    client.on('disconnect', disconnect);
+
+    function handshake({ id }) {
+      let exists = false;
+      if (id) {
+        exists = clients.includes(id);
+      }
+      if(!exists) {
+        id = uuid();
+        clientData[id] = {'secret': uuid()};
+        clients.push(id);
+        console.log(`New client, assiging id [${id}] data: [${JSON.stringify(clientData[id])}]`);
+      } else {
+        console.log(`Client [${id}] reconnected. Restoring data [${JSON.stringify(clientData[id])}]`);
+      }
+      client.id = id;
+      client.data = clientData[id];
+      client.emit('welcome', { id });
+    }
+
+    function disconnect() {
+      console.log(`Client [${client.id}] disconnected`);
+    }
   });
 }
 
